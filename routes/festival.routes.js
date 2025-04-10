a905864dd6d88c65f44fbd5a016d14f81c9fc0f6
Rafael G <rafaelgoncalo@icloud.com>
2025-04-10 12:00:32 +0200

    readOne created

--- routes/festival.routes.js
+++ routes/festival.routes.js
@@ -12,22 +12,39 @@ router.post('/create', async (req, res) => {
 } catch (error) {
     console.log('Well.... we didnt create anything');
     res.status(500).json({Error: 'Error creating Festival'});
-}
+};
 });
 
 // ******* GET/ READ route
 
-router.get('/festival', async (req,res) => {
+router.get('/read', async (req,res) => {
     try {
         const allFestival = await FestivalModel.find();
-        console.log('All festivals' Festival);
-        res.status(200).json(Festival);
+        console.log('All festivals', allFestival);
+        res.status(200).json(allFestival);
 
     } catch (error) {
         console.log('Error reading festivals');
         res.status(500).json({Error: 'Problem reading festivals'});
-
 }
 });
+
+// ******** GET ONE/ READ ONE route
+
+router.get('/read/:festivalId', async (req, res) => {
+    try {
+        const readOne = await FestivalModel.findById(req.params.festivalId)
+        console.log('One Festival Found');
+        res.status(200).json(readOne);
+
+    } catch (error) {
+        console.log('Problem Finding One');
+        res.status(500).json({Error: 'Problem Reading One'});
+    }
+});
+
+// ******* DELETE route
+
+
 
 module.exports = router;
